ebfbb3f63abb46ee6708de27755ebe92df9d919c
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-08-13 17:53:39 -0700

    task102

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -8,34 +8,35 @@ from pymongo import MongoClient
 def log_stats():
     """ log_stats.
     """
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-    total = logs_collection.count_documents({})
-    get = logs_collection.count_documents({"method": "GET"})
-    post = logs_collection.count_documents({"method": "POST"})
-    put = logs_collection.count_documents({"method": "PUT"})
-    patch = logs_collection.count_documents({"method": "PATCH"})
-    delete = logs_collection.count_documents({"method": "DELETE"})
-    path = logs_collection.count_documents(
-        {"method": "GET", "path": "/status"})
+    c = MongoClient('mongodb://localhost:27017/')
+    db = c['logs']
+    co = db['nginx']
+    total = co.count_documents({})
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    method_c = {method: co.count_documents({'method': method}) for method in methods}
+    status = co.count_documents({'method': 'GET', 'path': '/status'})
+    ip_counts = co.aggregate([
+        {
+            '$group': {
+                '_id': '$ip',
+                'count': {'$sum': 1}
+            }
+        },
+        {
+            '$sort': {'count': -1}
+        },
+        {
+            '$limit': 10
+        }
+    ])
     print(f"{total} logs")
     print("Methods:")
-    print(f"\tmethod GET: {get}")
-    print(f"\tmethod POST: {post}")
-    print(f"\tmethod PUT: {put}")
-    print(f"\tmethod PATCH: {patch}")
-    print(f"\tmethod DELETE: {delete}")
-    print(f"{path} status check")
+    for method in methods:
+        print(f"\tmethod {method}: {method_c[method]}")
+    print(f"{status} status check")
     print("IPs:")
-    sorted_ips = logs_collection.aggregate(
-        [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-         {"$sort": {"count": -1}}])
-    i = 0
-    for s in sorted_ips:
-        if i == 10:
-            break
-        print(f"\t{s.get('_id')}: {s.get('count')}")
-        i += 1
+    for ip in ip_counts:
+        print(f"\t{ip['_id']}: {ip['count']}")        i += 1
 
 
 if __name__ == "__main__":
