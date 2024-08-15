2effaec3feccec61977babe7b1164435f4b3ea5a
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-08-15 00:10:21 +0000

    task

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,19 +3,20 @@
 import uuid
 import redis
 from typing import Union, Optional, Callable
+from functools import wraps
 
 
 class Cache:
     """
-    storing data.
+    A cache class that stores data in Redis and provides methods for retrieval with conversion.
     """
 
     def __init__(self):
         """
-        Initializes the cache
+        Initializes the cache with a Redis connection.
         """
         self._redis = redis.Redis()
-        self._redis.flushdb()
+        self.method_calls = {}  # Dictionary to store method call counts
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
@@ -24,7 +25,8 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-    
+
+    @count_calls
     def get(self, key: str, fn: Optional[Callable] = None) -> Optional[Union[str, int]]:
         """
         Retrieves data and applies an  conversion .
@@ -39,6 +41,7 @@ class Cache:
             return int(data)
         except ValueError:
             return data.decode("utf-8")
+
     def get_str(self, key: str) -> Optional[str]:
         """
         Retrieves data  and converts it to a string.
@@ -52,3 +55,16 @@ class Cache:
         """
 
         return self.get(key, fn=int)
+
+    def count_calls(self, method):
+        """
+        counts calls to a method and returns a wrapped function.
+        """
+
+        @wraps(method)
+        def wrapper(self, *args, **kwargs):
+            method_name = method.__qualname__
+            self.method_calls[method_name] = self.method_calls.get(method_name, 0) + 1
+            return method(self, *args, **kwargs)
+
+        return wrapper
