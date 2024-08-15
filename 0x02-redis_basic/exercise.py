5e62488b8664495078127a67f35b81cdb71463d5
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-08-15 00:02:38 +0000

    task

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-""" task9 """
+""" tasks """
 import uuid
 import redis
-from typing import Union
+from typing import Union, Optional, Callable
 
 
 class Cache:
@@ -24,3 +24,31 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+    
+    def get(self, key: str, fn: Optional[Callable] = None) -> Optional[Union[str, int]]:
+        """
+        Retrieves data and applies an  conversion .
+        """
+
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        try:
+            return int(data)
+        except ValueError:
+            return data.decode("utf-8")
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Retrieves data  and converts it to a string.
+        """
+
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Retrieves data converts it to an integer.
+        """
+
+        return self.get(key, fn=int)
