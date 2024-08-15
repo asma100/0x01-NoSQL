5ab9d52fb51cea084501abcb5dc82e096c305c85
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-08-15 00:17:58 +0000

    task

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,21 +3,36 @@
 import uuid
 import redis
 from typing import Union, Optional, Callable
-from functools import wraps
+import functools
+
+
+def count_calls(method: Callable) -> Callable:
+    """
+    Decorator that counts the number of times a method is called.
+    """
+
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache:
     """
-    A cache class that stores data in Redis and provides methods for retrieval with conversion.
+    Storing data in Redis.
     """
 
     def __init__(self):
         """
-        Initializes the cache with a Redis connection.
+        Initializes the cache.
         """
         self._redis = redis.Redis()
-        self.method_calls = {}  # Dictionary to store method call counts
+        self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Stores data in Redis and returns the generated key.
@@ -26,12 +41,10 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    @count_calls
     def get(self, key: str, fn: Optional[Callable] = None) -> Optional[Union[str, int]]:
         """
-        Retrieves data and applies an  conversion .
+        Retrieves data and applies a conversion.
         """
-
         data = self._redis.get(key)
         if data is None:
             return None
@@ -44,27 +57,12 @@ class Cache:
 
     def get_str(self, key: str) -> Optional[str]:
         """
-        Retrieves data  and converts it to a string.
+        Retrieves data and converts it to a string.
         """
-
         return self.get(key, fn=lambda d: d.decode("utf-8"))
 
     def get_int(self, key: str) -> Optional[int]:
         """
-        Retrieves data converts it to an integer.
+        Retrieves data and converts it to an integer.
         """
-
         return self.get(key, fn=int)
-
-    def count_calls(self, method):
-        """
-        counts calls to a method and returns a wrapped function.
-        """
-
-        @wraps(method)
-        def wrapper(self, *args, **kwargs):
-            method_name = method.__qualname__
-            self.method_calls[method_name] = self.method_calls.get(method_name, 0) + 1
-            return method(self, *args, **kwargs)
-
-        return wrapper
